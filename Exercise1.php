fd3d12f9ce032d2ac8a155c3bec2c9bdac5328b5
Your Name <you@example.com>
2022-09-15 14:17:11 -0800

    Uploaded

---
+++ Exercise1.php
@@ -0,0 +1,10 @@
+<?php 
+
+	$twinkle = "Twinkle";
+	$star="Star";
+	echo "$twinkle, $twinkle little $star.\n";
+
+	$twinkle = "hello";
+	$star = "world";
+	echo "$twinkle, $twinkle little $star.\n";
+?>
