8a1918bc88eb78feed63b0dbea074acebce2397a
Gemma-lyn Cuilan <92642445+GemmalynCuilan@users.noreply.github.com>
2022-11-15 18:38:33 -0800

    Update PHP Exercise 4.php

--- PHP Exercise 4.php
+++ PHP Exercise 4.php
@@ -3,13 +3,13 @@
 		$age=28;   
  
 			var_dump($name); 
-			echo "<br/>";   
+			echo "\n";   
  
 			print_r($name); 
-			echo "<br/>";   
+			echo "\n";   
  
 			var_dump($age); 
-			echo "<br/>";  
+			echo "\n";  
  
 		$name=null;
 
